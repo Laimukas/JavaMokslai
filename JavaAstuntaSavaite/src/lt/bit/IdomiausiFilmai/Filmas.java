2a6bbe62126e04eba83c3a816e46c3b56ce2bbb3
Laimukas <l.kestauskas@gmail.com>
2022-05-26 21:59:05 +0300

    ketv pamokos su Dziugu

--- JavaAstuntaSavaite/src/lt/bit/IdomiausiFilmai/Filmas.java
+++ JavaAstuntaSavaite/src/lt/bit/IdomiausiFilmai/Filmas.java
@@ -19,16 +19,20 @@ package lt.bit.IdomiausiFilmai;
 //3. sukurkite rikiavimo pagal du raktus procedūrą (funkciją void C++);
 //4. sukurkite rezultatų rašymo į failą procedūrą (funkciją void C++).
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalTime;
+import java.util.List;
 
-public class Filmas {
+public class Filmas implements Comparable<Filmas>{
 
     private String pavadinimas;
-    private int metai;
+    private Integer metai;
     private LocalTime trukme;
-    private int ziurovuSkaiciusMln;
+    private Integer ziurovuSkaiciusMln;
 
-    public Filmas(String pavadinimas, int metai, LocalTime trukme, int ziurovuSkaiciusMln) {
+    public Filmas(String pavadinimas, Integer metai, LocalTime trukme, Integer ziurovuSkaiciusMln) {
         this.pavadinimas = pavadinimas;
         this.metai = metai;
         this.trukme = trukme;
@@ -43,11 +47,11 @@ public class Filmas {
         this.pavadinimas = pavadinimas;
     }
 
-    public int getMetai() {
+    public Integer getMetai() {
         return metai;
     }
 
-    public void setMetai(int metai) {
+    public void setMetai(Integer metai) {
         this.metai = metai;
     }
 
@@ -59,11 +63,11 @@ public class Filmas {
         this.trukme = trukme;
     }
 
-    public int getZiurovuSkaiciusMln() {
+    public Integer getZiurovuSkaiciusMln() {
         return ziurovuSkaiciusMln;
     }
 
-    public void setZiurovuSkaiciusMln(int ziurovuSkaiciusMln) {
+    public void setZiurovuSkaiciusMln(Integer ziurovuSkaiciusMln) {
         this.ziurovuSkaiciusMln = ziurovuSkaiciusMln;
     }
 
@@ -74,6 +78,40 @@ public class Filmas {
                 ", metai=" + metai +
                 ", trukme=" + trukme +
                 ", ziurovuSkaiciusMln=" + ziurovuSkaiciusMln +
-                '}';
+                "}\n";
     }
+    //1 - dabartinis filmas yra auksciau Filmas o
+    //0 - abu filmai pagal rikiavima lygus
+    //-1 - dabartinis filmas yra zemiau Filmas o
+    @Override
+    public int compareTo(Filmas o) {
+        //surikiuoti pagal trukme didejancia tvarka
+        int res = trukme.compareTo(o.getTrukme());
+        // ------ Variantas 1 ------------
+//        //cia didejancia
+//        if(res > 0){
+//            return 1;
+//        }else if (res <0){
+//            return -1;
+//        }
+//        //jei lygu pagal ziurovuSkaiciuMln mazejancia tvarka
+//        res = ziurovuSkaiciusMln.compareTo(o.getZiurovuSkaiciusMln());
+//        //cia mazejancia
+//        if(res > 0){
+//            return -1;
+//        }else if (res <0){
+//            return 1;
+//        }
+//        return 0;
+
+        //----------Variantas 2 ------------------
+
+        if(res !=0) {
+            return res;
+        }
+        //jei lygu pagal ziurovuSkaiciuMln mazejancia tvarka
+        return -ziurovuSkaiciusMln.compareTo(o.getZiurovuSkaiciusMln());
+    }
+
+
 }
--- JavaAstuntaSavaite/src/lt/bit/IdomiausiFilmai/Main.java
+++ JavaAstuntaSavaite/src/lt/bit/IdomiausiFilmai/Main.java
@@ -1,28 +1,46 @@
 package lt.bit.IdomiausiFilmai;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import javax.naming.directory.SearchResult;
+import java.io.*;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         //nurodome duomenu failo kelia
         String duomenuFailoKelias = "JavaAstuntaSavaite/src/lt/bit/IdomiausiFilmai/duomenys.txt";
+        String rezultatuFailoKelias = "JavaAstuntaSavaite/src/lt/bit/IdomiausiFilmai/rezultatai.txt";
         //iskvieciam nuskaitymo metoda
         List<Filmas> filmai = skaityti(duomenuFailoKelias);
         System.out.println(filmai);
         //surikiuot filmus
-
+        Collections.sort(filmai);
+        System.out.println(filmai);
         //atspausdinti i faila
+        irasytiFilmusFailan(rezultatuFailoKelias,filmai);
+
+        //surikiuoti naudojant anonimine klase
+        //pagal pavadinima,jei lygu pagal metus,jei lygu pagal ziurovuSkaiciusMln
+        Collections.sort(filmai, new Comparator<Filmas>() {
+            @Override
+            public int compare(final Filmas o1,final Filmas o2) {
+                int res = o1.getPavadinimas().compareToIgnoreCase(o2.getPavadinimas());
+                if (res != 0) {
+                    return res;
+                }
+            return o1.getZiurovuSkaiciusMln().compareTo(o2.getZiurovuSkaiciusMln());
+            }
+        });
+        System.out.println(filmai);
     }
-    public static List<Filmas> skaityti(String duomenuFailoKelias){
+
+    public static List<Filmas> skaityti(String duomenuFailoKelias) {
         List<Filmas> filmai = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(duomenuFailoKelias))){
+        try (BufferedReader br = new BufferedReader(new FileReader(duomenuFailoKelias))) {
             String eilute = br.readLine();
             int filmuKiekis = Integer.parseInt(eilute);
             System.out.println("filmu kiekis: " + filmuKiekis);
@@ -33,24 +51,41 @@ public class Main {
 
                 int metai = Integer.parseInt(reiksmes[0]);
                 String filmoPavadinimas = eilute.substring(5, 29).trim();
-                LocalTime trukme = LocalTime.of(Integer.parseInt(reiksmes[reiksmes.length-3]),Integer.parseInt(reiksmes[reiksmes.length-2]));
-                int ziurovuKiekis = Integer.parseInt(reiksmes[reiksmes.length-1]);
+                int valandos = Integer.parseInt(reiksmes[reiksmes.length - 3]);
+                int minutes = Integer.parseInt(reiksmes[reiksmes.length - 2]);
+                LocalTime trukme = LocalTime.of(valandos, minutes);
+                Integer ziurovuKiekis = Integer.parseInt(reiksmes[reiksmes.length - 1]);
 
-                Filmas filmas = new Filmas(filmoPavadinimas,metai,trukme,ziurovuKiekis);
+                Filmas filmas = new Filmas(filmoPavadinimas, metai, trukme, ziurovuKiekis);
 
                 filmai.add(filmas);
             }
 
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         return filmai;
     }
-    public static void rikiuotiFilmus(List<Filmas> filmai){
-        for (int i =0;i< filmai.size();i++){
-            //kaip juos surikiuot :(
-       //   Collections.sort(filmai.get(i).getZiurovuSkaiciusMln(),Collections.sort(filmai.get(i).getMetai()));
+    //Rikiavimas
+//    Collections.sort(rowItems,new Comparator<SearchResult>){
+//        @Override
+//                public static compare(final SearchResult o1, final SearchResult o2){
+//            LocalTime compare = o1.
+//        }
+//    }
+    public static void irasytiFilmusFailan(String failoKelias, List<Filmas> filmai){
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter(failoKelias));
+            for (int i =0;i< filmai.size();i++){
+                Filmas filmas = filmai.get(i);
+
+                bw.write(String.format("%s %-29s %s %s",filmai.get(i).getMetai(),filmai.get(i).getPavadinimas(),"trukmė: "+filmai.get(i).getTrukme(),"; susirinko žiūrovų: "+filmai.get(i).getZiurovuSkaiciusMln()+" Mln."));
+                bw.newLine();
+            }
+            bw.close();
+        }catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
