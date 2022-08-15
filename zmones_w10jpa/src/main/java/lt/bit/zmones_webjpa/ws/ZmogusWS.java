5f9905f176a70bf6664a38a3d49c830f2b275da7
Laimukas <l.kestauskas@gmail.com>
2022-08-15 22:03:54 +0300

    Update

--- zmones_w10jpa/src/main/java/lt/bit/zmones_webjpa/ws/ZmogusWS.java
+++ zmones_w10jpa/src/main/java/lt/bit/zmones_webjpa/ws/ZmogusWS.java
@@ -1,21 +1,22 @@
 package lt.bit.zmones_webjpa.ws;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Query;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
+
 import java.util.List;
+
 import lt.bit.zmones_webjpa.data.Zmogus;
 
 @Path("zmogus")
 public class ZmogusWS {
-    
+
     @Context
     private HttpServletRequest request;
-    
+
     @GET
     @Produces("application/json")
     public List<Zmogus> list() {
@@ -23,12 +24,41 @@ public class ZmogusWS {
         Query q = em.createQuery("select z from Zmogus z order by z.vardas, z.pavarde");
         List<Zmogus> list = q.getResultList();
         return list;
-//        List<Zmogus> l = new ArrayList();
-//        Zmogus z;
-//        z = new Zmogus(1, "Jonas", "Jonaitis", null, BigDecimal.TEN);
-//        l.add(z);
-//        z = new Zmogus(2, "Petras", "Petraitis", new Date(), new BigDecimal("651.48"));
-//        l.add(z);
-//        return l;
+
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces("application/json")
+    public Zmogus getOne(@PathParam("id") Integer id) {
+        if (id == null) {
+            return null;
+        }
+        EntityManager em = (EntityManager) request.getAttribute("em");
+        Zmogus z = em.find(Zmogus.class, id);
+        return z;
+    }
+
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Zmogus add (Zmogus z){
+        System.out.println("Gavom nauja zmogu: "+ z);
+        if(z == null){
+            throw new NullPointerException("Nera ka issaugoti");
+        }
+        z.setId(null);
+        EntityManager em = (EntityManager) request.getAttribute("em");
+        EntityTransaction tx = em.getTransaction();
+        try{
+            tx.begin();
+            em.persist(z);
+            tx.commit();
+            return z;
+        } finally {
+            if (tx.isActive()){
+                tx.rollback();
+            }
+        }
     }
 }
